cdffa90a442d42fdf775ae97b3a576a56cdb168e
Izhak <izhaklatovski@gmail.com>
2021-02-18 20:01:35 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -61,3 +61,90 @@ $(document).ready(function(){
         $(this).addClass("active").siblings().removeClass("active");
     });
 });
+
+// Popup modal -----------------------------------------------------------------------------------
+
+// Project 1 -----
+// Get the modal
+var modal1 = document.getElementById("project1Modal");
+// When the user clicks on the button, open the modal
+document.getElementById("projectButton1").onclick = function() {
+  modal1.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+document.getElementsByClassName("close")[0].onclick = function() {
+  modal1.style.display = "none";
+}
+
+// Project 2 -----
+// Get the modal
+var modal2 = document.getElementById("project2Modal");
+// When the user clicks on the button, open the modal
+document.getElementById("projectButton2").onclick = function() {
+  modal2.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+document.getElementsByClassName("close")[1].onclick = function() {
+  modal2.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal1) {
+    modal1.style.display = "none";
+  } else if (event.target === modal2) {
+    modal2.style.display = "none";
+  }
+}
+
+// Photo gallery ----------------------------------------------------------------------------------
+// 1
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides1");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
+// 2
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+// Next/previous controls
+function plusSlides2(n) {
+    showSlides2(slideIndex2 += n);
+  }
+
+// Thumbnail image controls
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+  }
+
+function showSlides2(n) {
+    var i;
+    var slides2 = document.getElementsByClassName("mySlides2");
+    if (n > slides2.length) {slideIndex2 = 1}
+    if (n < 1) {slideIndex2 = slides2.length}
+    for (i = 0; i < slides2.length; i++) {
+        slides2[i].style.display = "none";
+    }
+    slides2[slideIndex2-1].style.display = "block";
+  }
+
